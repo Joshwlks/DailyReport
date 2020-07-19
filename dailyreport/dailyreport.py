67253ee53a618442a99c223e34203c66cc5efaae
Lackshan <lackshan.rama@gmail.com>
2020-07-19 23:27:01 +0100

    Updated dailyreport to read csv data from UK Gov's covid website. Created dummy file.

--- dailyreport/dailyreport.py
+++ dailyreport/dailyreport.py
@@ -3,6 +3,7 @@ import pandas as pd
 covid_daily_case_url = 'https://coronavirus.data.gov.uk/downloads/csv/coronavirus-cases_latest.csv'
 covid_daily_death_url = 'https://coronavirus.data.gov.uk/downloads/csv/coronavirus-deaths_latest.csv'
 
-daily_case_df = pandas.read
+daily_case_df = pd.read_csv(covid_daily_case_url)
+daily_case_df = pd.read_csv(covid_daily_death_url)
 
 
