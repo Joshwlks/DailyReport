a7892ba1fa5a7a9b466a05fa78a30a7a82e878f3
Lackshan <lackshan.rama@gmail.com>
2020-07-19 23:25:31 +0100

    Created dailyreport.py

---
+++ dailyreport/dailyreport.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+covid_daily_case_url = 'https://coronavirus.data.gov.uk/downloads/csv/coronavirus-cases_latest.csv'
+covid_daily_death_url = 'https://coronavirus.data.gov.uk/downloads/csv/coronavirus-deaths_latest.csv'
+
+daily_case_df = pandas.read
+
+
