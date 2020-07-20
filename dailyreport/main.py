d81eddf4f5238acd741e7f117e462d17eaeb5ba1
Lackshan <lackshan.rama@gmail.com>
2020-07-20 16:22:04 +0100

    Fixed formatting.

--- dailyreport/main.py
+++ dailyreport/main.py
@@ -10,24 +10,24 @@ york_daily_case_df = covid_data_tuple[2]
 york_daily_death_df = covid_data_tuple[3]
 
 latex_jinja_env = jinja2.Environment(
-	block_start_string = '\BLOCK{',
-	block_end_string = '}',
-	variable_start_string = '\VAR{',
-	variable_end_string = '}',
-	comment_start_string = '\#{',
-	comment_end_string = '}',
-	line_statement_prefix = '%%',
-	line_comment_prefix = '%#',
-	trim_blocks = True,
-	autoescape = False,
-	loader = jinja2.FileSystemLoader(os.path.abspath('.'))
+	block_start_string='\BLOCK{',
+	block_end_string='}',
+	variable_start_string='\VAR{',
+	variable_end_string='}',
+	comment_start_string='\#{',
+	comment_end_string='}',
+	line_statement_prefix='%%',
+	line_comment_prefix='%#',
+	trim_blocks=True,
+	autoescape=False,
+	loader=jinja2.FileSystemLoader(os.path.abspath('.'))
 )
 
-#gets the reportTemplate
+# Gets the reportTemplate
 template = latex_jinja_env.get_template('./latex_templates/reportTemplate.tex')
-#loads the data into the template
-document = template.render(gdcases = str(england_daily_case_df))
-#write document
+# Loads the data into the template
+document = template.render(gdcases=str(england_daily_case_df))
+# Write document
 with open("dailyReport.tex", "w") as output:
-    output.write(document)
+	output.write(document)
 
