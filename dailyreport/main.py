d51729763d29636429d036e5bd0c0d9d4f639c96
Joshua Wilkins <joshwlks98@gmail.com>
2020-07-20 01:30:42 +0100

    Created LaTeX template with a variable 'gdcase' which is read in the main.py file and then replaced with the england daily case data.

--- dailyreport/main.py
+++ dailyreport/main.py
@@ -1,8 +1,33 @@
 import dailyreport.datasources.covid19.covid as covid
+import jinja2
+import os
+from jinja2 import Template
 
 covid_data_tuple = covid.get_covid_data()
 england_daily_case_df = covid_data_tuple[0]
 england_daily_death_df = covid_data_tuple[1]
 york_daily_case_df = covid_data_tuple[2]
 york_daily_death_df = covid_data_tuple[3]
+
+latex_jinja_env = jinja2.Environment(
+	block_start_string = '\BLOCK{',
+	block_end_string = '}',
+	variable_start_string = '\VAR{',
+	variable_end_string = '}',
+	comment_start_string = '\#{',
+	comment_end_string = '}',
+	line_statement_prefix = '%%',
+	line_comment_prefix = '%#',
+	trim_blocks = True,
+	autoescape = False,
+	loader = jinja2.FileSystemLoader(os.path.abspath('.'))
+)
+
+#gets the reportTemplate
+template = latex_jinja_env.get_template('./latex_templates/reportTemplate.tex')
+#loads the data into the template
+document = template.render(gdcases = str(england_daily_case_df))
+#write document
+with open("dailyReport.tex", "w") as output:
+    output.write(document)
 
